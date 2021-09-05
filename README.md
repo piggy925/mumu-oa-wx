c2861a8e662849a4c7f0a2e8da66abe4a14152bb
MuMu <piggy925@163.com>
2021-09-05 22:35:55 +0800

    feat：完成登录页面

---
+++ README.md
@@ -0,0 +1,4 @@
+# mumu-oa-wx
+基于spring-boot的在线协同办公微信小程序前端页面
+
+后端api项目地址：https://github.com/piggy925/mumu-oa
---
+++ pages/demo/demo.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+		<view>{{username}}</view>
+		<view v-for="tel in tels">{{tel}}</view>
+		<view v-if="age > 18">
+			<button @tap="signUp()">点击报名</button>
+		</view>
+		<view>
+			<input type="text" v-model="address" placeholder="test" />
+		</view>
+		<view>{{address}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				username: "MuMu",
+				tels: ["123", "456", "789"],
+				age: 20,
+				address: ""
+			}
+		},
+		methods: {
+			signUp: function(){
+				uni.showToast({
+					title:"点击了报名按钮"
+				})
+			}
+			
+		}
+	}
+</script>
+
+<style>            
+
+</style>
