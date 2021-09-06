2531ef00bc189807e826b12837245cd16cbbd1d5
MuMu <piggy925@163.com>
2021-09-06 12:13:26 +0800

    feat：获取用户信息

--- pages/register/register.vue
+++ pages/register/register.vue
@@ -2,9 +2,9 @@
 	<view>
 		<image src="../../static/logo-2.png" mode="widthFix" class="logo"></image>
 		<view class="register-container">
-			<input type="number" class="register-code" placeholder="输入你的邀请码" maxlength="6" v-model="registerCode"/>
+			<input type="number" class="register-code" placeholder="输入你的邀请码" maxlength="6" v-model="registerCode" />
 			<view class="register-desc">管理员创建员工账号之后，你可以从你的个人邮箱种获得注册邀请码</view>
-			<button class="btn-register" open-type="getUserInfo" @tap="register()">执行注册</button>
+			<button class="btn-register" @click="register()">执行注册</button>
 		</view>
 	</view>
 </template>
@@ -18,7 +18,21 @@
 			}
 		},
 		methods: {
+			register: function() {
+				uni.login({
+					provider: "weixin",
+					success: function(resp) {
+						console.log(resp.code)
+					}
+				})
+				uni.getUserProfile({
+					desc: "weixin",
+					success: function(resp) {
+						console.log(resp)
+					}
 
+				});
+			}
 		}
 	}
 </script>
